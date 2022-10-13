597f9ed397f6fc301277e85a76c541a027522eea
Fei Xin Duke <113947184+FXDuke@users.noreply.github.com>
2022-10-13 10:11:01 -0400

    Update main.js

--- ConjuegamosBot/main.js
+++ ConjuegamosBot/main.js
@@ -13,27 +13,27 @@ Settings['On'] = settingData_Split[2];
 Settings['Max'] = settingData_Split[3];
 
 async function start(text) {
-    var browser = await puppeteer.launch({headless: false});
-    var page = await browser.newPage();
-    await page.goto("https://conjuguemos.com/auth/login/", {waitUntil: 'networkidle0',});
-    await page.type("#identity",Settings['Username']);
-    await page.type("#password",Settings['Password']);
-    await page.type("#password","\n");
-    await page.goto("https://conjuguemos.com/student/activities", {waitUntil: 'networkidle0',});
-    var vocab_lists = await page.evaluate(() => {
-      var final = [];
-      var Vocab_lists_index = document.getElementsByClassName("js-filter-text");
-      var __Index = 0;
-      for (List_data of Vocab_lists_index) {
-        final[__Index] = [List_data.innerHTML.split("\t")[7],List_data.href,List_data.href.split("/")[List_data.href.split("/").length-1]];
-        __Index++;
-      }
-      return final;
-   })
-    var vocab_list_id = 0;
-    for (list of vocab_lists) {if (list[0] == text) {vocab_list_id = list[2]}}
-    await page.goto("https://conjuguemos.com/vocabulary/print_flashcards/" + vocab_list_id, {waitUntil: 'networkidle0',})
-    var Vocab_List = await page.evaluate(() => { 
+  var browser = await puppeteer.launch({headless: false});
+  var page = await browser.newPage();
+  await page.goto("https://conjuguemos.com/auth/login/", {waitUntil: 'networkidle0',});
+  await page.type("#identity",Settings['Username']);
+  await page.type("#password",Settings['Password']);
+  await page.type("#password","\n");
+  await page.goto("https://conjuguemos.com/student/activities", {waitUntil: 'networkidle0',});
+  var vocab_lists = await page.evaluate(() => {
+    var final = [];
+    var Vocab_lists_index = document.getElementsByClassName("js-filter-text");
+    var __Index = 0;
+    for (List_data of Vocab_lists_index) {
+      final[__Index] = [List_data.innerHTML.split("\t")[7],List_data.href,List_data.href.split("/")[List_data.href.split("/").length-1]];
+      __Index++;
+    }
+    return final;
+  })
+  var vocab_list_id = 0;
+  for (list of vocab_lists) {if (list[0] == text) {vocab_list_id = list[2]}}
+  await page.goto("https://conjuguemos.com/vocabulary/print_flashcards/" + vocab_list_id, {waitUntil: 'networkidle0',})
+  var Vocab_List = await page.evaluate(() => { 
     var final2 = [];
     var question = document.getElementsByClassName("qWord")
     var answer = document.getElementsByClassName("qDef")
@@ -44,14 +44,14 @@ async function start(text) {
       __Index++;
     }
     return final2;
-   })
-    await page.goto("https://conjuguemos.com/vocabulary/homework/" + vocab_list_id, {waitUntil: 'networkidle0',})
-    var temp = await page.evaluate(() => {return document.getElementsByClassName("btn--start-gp btn")[0].id = "botclickme"})
-    await sleep(1000)
-    await page.click("#botclickme")
-    await sleep(3000)
-    var __loopIndex = 0;
-    async function loop() {
+  })
+  await page.goto("https://conjuguemos.com/vocabulary/homework/" + vocab_list_id, {waitUntil: 'networkidle0',})
+  var temp = await page.evaluate(() => {return document.getElementsByClassName("btn--start-gp btn")[0].id = "botclickme"})
+  await sleep(1000)
+  await page.click("#botclickme")
+  await sleep(3000)
+  var __loopIndex = 0;
+  async function loop() {
     var question_input = await page.evaluate(() => {return document.querySelector("#question-input").innerHTML})
     var final_answer = "";
     for (vocab_data of Vocab_List) {if (vocab_data[0]==question_input) {final_answer=vocab_data[1]}}
