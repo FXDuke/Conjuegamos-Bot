5cd1730095d5297b322ed55f135ebabe97776d50
Fei Xin Duke <113947184+FXDuke@users.noreply.github.com>
2022-10-13 11:37:03 -0400

    v0.0.1

--- ConjuegamosBot/main.js
+++ ConjuegamosBot/main.js
@@ -20,10 +20,12 @@ Settings['Max'] = settingData_Split[3];
 
 /* Main Function (start) */
 
-async function start(text) {
+async function start(text,time) {
+
+  time = Math.round(time)
 
   /* Establishing Connection */
-
+  
   var browser = await puppeteer.launch({headless: false});
   var page = await browser.newPage();
 
@@ -50,7 +52,7 @@ async function start(text) {
   /* Finding the List's ID */
 
   var vocab_list_id = 0;
-  for (list of vocab_lists) {if (list[0] == text) {vocab_list_id = list[2]}}
+  for (list of vocab_lists) {if (list[0] == text) {vocab_list_id = list[2]};};
 
   /* Getting the list's Vocab Answers */
 
@@ -69,30 +71,55 @@ async function start(text) {
   });
 
   /* Starting the assignment */
-
+  
+  let time_to_wait = 0;
   await page.goto("https://conjuguemos.com/vocabulary/homework/" + vocab_list_id, {waitUntil: 'networkidle0'});
   var temp = await page.evaluate(() => {return document.getElementsByClassName("btn--start-gp btn")[0].id = "botclickme"});
   await sleep(1000);
+  await page.click("#set_time_input")
+  await page.keyboard.press('Backspace')
+  await page.type("#set_time_input", time.toString())
   await page.click("#botclickme");
-  await sleep(3000);
+  await sleep(2000);
   var __loopIndex = 0;
+  var __maxwanted = (Settings['Max']!=null) ? Settings['Max']-1 : 10000;
+  var old = new Date();
 
   /* Initializing the bot */
 
-  async function loop() {
+  var current = new Date();
+  while (current.getSeconds()-old.getSeconds() < time*60) {
+    current = new Date();
     const question_input = await page.evaluate(() => {return document.querySelector("#question-input").innerHTML});
     var final_answer = "";
     for (vocab_data of Vocab_List) {if (vocab_data[0]==question_input) {final_answer=vocab_data[1]};};
     await page.type("#answer-input",final_answer + "\n");
-    const maxwanted_2 = (Settings['Max']!=null) ? Settings['Max']-1 : __loopIndex + 1;
-    if (__loopIndex<maxwanted_2) {
-      loop();
+    if (__loopIndex<__maxwanted) {
       __loopIndex++;
+    } else {
+      time_to_wait = (time*1000*60)-((current.getSeconds()-old.getSeconds())*1000)
+      break;
     };
   };
-  loop();
+
+  /* Saving the score */
+
+  await sleep(time_to_wait + 1500)
+  var temp2 = await page.evaluate(() => {
+    for (button of document.getElementsByClassName("btn")) {
+      if (button.innerHTML == "Save") {
+        button.id = "newbuttonbotclick"
+      };
+    };
+    return document.getElementById("newbuttonbotclick")
+  });
+  await page.click("#newbuttonbotclick")
+
+  /* Closing */
+
+  await browser.close()
 };
 
 /* Starting */
 
-start(Settings['On']);
+start(Settings['On'],2.5);
